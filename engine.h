d6ecc901d4184824562471242022083c7eb621a6
Igor Janjic <ijanjic@vt.edu>
2012-04-19 18:12:57 -0300

    Update engine.h.

---
+++ engine.h
@@ -0,0 +1,23 @@
+/***************************************************************************************************
+ File Name:
+  engine.h
+
+ Purpose:
+	This file includes all of the header files required for the engine to function correctly.
+
+ Authors:
+	Igor Janjic
+***************************************************************************************************/
+
+#ifndef ENGINE_H
+#define ENGINE_H
+
+// Memory manager
+#include "smointer.h"
+#include "blob.h"
+#include "dblob.h"
+
+// Kernel
+// Files...
+
+#endif
